61061ef9985a667e8d38eedd687e7963314a5aa3
201710787 <egnachio@naver.com>
2023-01-30 18:12:03 +0900

    DEBUGGING: filehandler router

--- routes/fileHandler.ts
+++ routes/fileHandler.ts
@@ -6,11 +6,10 @@ const fs = require("fs/promises");
 
 /* GET home page. */
 router.route("/").post(async (req: Request, res: Response) => {
-  const mime = JSON.stringify(req.body.file);
-  const typeIdx = mime.indexOf("/");
-  const base64Idx = mime.substring(typeIdx + 1).indexOf("/");
-  const base64 = mime.substring(typeIdx + base64Idx + 1);
-  const buffer = Buffer.from(base64.substring(0, base64.length - 1), "base64");
+  const mime = req.body.file
+  const base64Idx = mime.indexOf(",");
+  const base64 = mime.substring(base64Idx + 1);
+  const buffer = Buffer.from(base64, "base64");
   await fs.writeFile(req.body.path, buffer);
   res.end();
 });
